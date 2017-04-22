b454bc75584d692992d25d230fff3390b4b959e6
mihailpaleologu <mihailpaleologu@yahoo.com>
2017-04-22 13:09:09 +0300

    Update application_helper.rb

--- app/helpers/spina/application_helper.rb
+++ app/helpers/spina/application_helper.rb
@@ -1,6 +1,16 @@
 module Spina
   module ApplicationHelper
+          def resource_name
+             :user
+           end
 
+           def resource
+             @resource ||= User.new
+           end
+
+           def devise_mapping
+             @devise_mapping ||= Devise.mappings[:user]
+           end
     def current_account
       @current_account ||= Account.first
     end
